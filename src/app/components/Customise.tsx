4ecd9312b52667a1ff1742c315ed8357faa648e6
Your Name <you@example.com>
2024-11-12 00:49:32 +0500

    assignment-03

--- src/app/components/Customise.tsx
+++ src/app/components/Customise.tsx
@@ -1,21 +1,22 @@
+import React from 'react';
 
-const Customise = () => {
+const Customise: React.FC = () => {
   return (
     <>
-    <div className="px-[220px] py-[140px] w-[1921px] h-[px] top-[3268px] bg-[#ffffff] flex items-center justify-between font-Inter">
+      <div className="px-[220px] py-[140px] w-[1921px] h-[px] top-[3268px] bg-[#ffffff] flex items-center justify-between font-Inter">
         <div className="w-[714px] h-[532.09px] bg-[#C4DEFD]"></div>
         <div className="w-[699px] h-[411px]">
           <div className="w-[669px] h-[288px]">
             <h2 className="font-bold text-[72px] leading-[87.14px] tracking-[-0.02em] mb-6 text-[#212529]">
-                Customise it
-                to <br /> your needs
+              Customise it
+              to <br /> your needs
             </h2>
             <p className="font-[400] text-lg leading-[30px] tracking-[-0.02em] text-[#212529] mb-6">
-            Customise the app with plugins, custom themes and multiple text editors (Rich <br /> Text or Markdown). Or create your own scripts and plugins using the Extension <br /> API.
+              Customise the app with plugins, custom themes and multiple text editors (Rich <br /> Text or Markdown). Or create your own scripts and plugins using the Extension <br /> API.
             </p>
             <br />
             <button className="bg-[#4F9CF9] py-5 px-10 font-medium text-lg tracking-[-0.02em] rounded-md text-[#ffffff]">
-                Let's Go <span> <svg
+              Let&apos;s Go <span> <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
@@ -34,7 +35,7 @@ const Customise = () => {
         </div>
       </div>
     </>
-  )
+  );
 }
 
-export default Customise
+export default Customise;
--- src/app/components/Extensions.tsx
+++ src/app/components/Extensions.tsx
@@ -1,20 +1,20 @@
+import React from 'react';
 
-const Extensions = () => {
+const Extensions: React.FC = () => {
   return (
     <>
-     <div className="px-[220px] py-[140px] w-[1921px] h-[759px] top-[92px] bg-[#043873] text-[#ffffff] flex items-center justify-between font-Inter">
+      <div className="px-[220px] py-[140px] w-[1921px] h-[759px] top-[92px] bg-[#043873] text-[#ffffff] flex items-center justify-between font-Inter">
         <div className="w-[697px] h-[264px]">
           <div className="w-[697px] h-[171px]">
             <h2 className="font-bold text-[72px] leading-[87.14px] tracking-[-0.02em] mb-6">
-                Use as Extension
+              Use as Extension
             </h2>
             <p className="font-[400] text-lg leading-[30px] tracking-[-0.02em] mb-10">
-            Use the web clipper extension, available on Chrome and Firefox, to save web pages <br /> or take screenshots as notes.
-
+              Use the web clipper extension, available on Chrome and Firefox, to save web pages <br /> or take screenshots as notes.
             </p>
             <br />
             <button className="bg-[#4F9CF9] py-5 px-10 font-medium text-lg tracking-[-0.02em] rounded-md">
-                Let's Go <span> <svg
+              Let&apos;s Go <span> <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
@@ -34,7 +34,7 @@ const Extensions = () => {
         <div className="w-[686px] h-[479px] bg-[#C4DEFD]"></div>
       </div>
     </>
-  )
+  );
 }
 
-export default Extensions
+export default Extensions;
